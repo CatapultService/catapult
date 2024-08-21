8158fa866a90363d6f9514fdc03ec2c04877d1a0
Nishant Mishra <nishdrummer@gmail.com>
2024-08-21 21:27:06 +0530

    feat: added people section

--- components/layout/people.tsx
+++ components/layout/people.tsx
@@ -4,15 +4,16 @@ import People from "@/public/assets/people/people.png";
 
 export default function people() {
   return (
-    <div className=" bg-white px-10 md:px-20 text-black items-center">
-      <div className="mx-auto min-w-6xl mb-8 px-0 xl:px-14">
+    <div className="flex bg-white px-10 md:px-20 mb-10 text-[#202020]">
+        <div className="mx-auto max-w-6xl ">
+      {/* <div className="mx-auto min-w-6xl mb-8 px-0 xl:px-14"> */}
         <div className="flex flex-col mt-10">
           <p className="text-sm text-left sm:text-lg md:text-2xl lg:text-3xl font-bold text-[#090E4A] ">
             OUR PEOPLE
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 ">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 md:gap-16 ">
           <div className="">
             <div className="flex flex-col lg:flex-row items-center my-6 gap-4">
               <img src={People.src} alt="people" className="w-40 h-full" />
