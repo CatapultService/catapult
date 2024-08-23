96c0c5954c89db9a35e5d9706f66e1a174705029
Nishant Mishra <nishdrummer@gmail.com>
2024-08-24 00:15:09 +0530

    feat: Added Advatage page

--- app/catapult-advantage/_components/layout/insights.tsx
+++ app/catapult-advantage/_components/layout/insights.tsx
@@ -3,21 +3,17 @@ import Food from "@/public/assets/aboutUs/food.png";
 import People from "@/public/assets/aboutUs/people.png";
 import Work from "@/public/assets/aboutUs/work.png";
 
-
 export default function TextPage() {
   return (
     <>
       <div className=" bg-white px-10 md:px-20 text-black flex items-center">
         <div className="mx-auto max-w-6xl my-5">
           <p className="mt-16 mb-8 text-base leading-relaxed text-left">
-            Catapult is a &quot;Social Enterprise Platform&quot; that is
-            committed to both sides of the equation. We want to build lasting
-            customer relationships by driving change & creating value. Equally
-            we are committed to nurtuting talent in an inclusive way that
-            creates a sustainable growth model.
+            Our Unique Insights as a Service Model for Digital Transformation
+            solves for the typical barriers to startng this journey :
           </p>
 
-          <div className="flex flex-col mb-8">
+          {/* <div className="flex flex-col mb-8">
             <p className="text-sm sm:text-lg md:text-2xl lg:text-3xl mb-6 font-bold text-[#090E4A] text-left">
               Our Aspiration - Sustainable Value Creation through Inclusivity
             </p>
@@ -47,97 +43,82 @@ export default function TextPage() {
               Through our commitment to diversity and equality, we strive to
               build a better future for all.
             </p>
-          </div>
+          </div> */}
 
           <div className="flex flex-col mb-8">
-            <p className="text-sm mt-3 sm:text-lg md:text-2xl lg:text-3xl mb-6 font-bold text-[#090E4A] text-left">
-              Our Unique Talent Management Model
-            </p>
-            <p className=" text-base leading-relaxed  text-left">
-              A hub and spoke model , with flexibility of pods being located in
-              Tier 2 - 3 towns and are led by talent internally developed.
-            </p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 ">
+              <div className="px-6 py-2 rounded-lg">
+                <h2 className="text-xl font-bold mb-2 bg-[#090F4A] text-white py-3 px-2">
+                  Budget
+                </h2>
+                <p className="text-base leading-relaxed text-left pt-2">
+                  Digital Transformation can cost millions in Upfront
+                  implemntation costs and ongoing license fees. Our unique
+                  Insights as a Service model eliminates cost barriers.{" "}
+                </p>
+              </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
               <div className=" px-6 py-2 rounded-lg">
-                <h2 className="text-xl font-bold mb-2 text-[#090F4A]">
-                  Hiring Criteria
+                <h2 className="text-xl font-bold mb-2 bg-[#090F4A] text-white py-3 px-2">
+                  People
                 </h2>
-                <ul className="text-gray-700">
-                  <li>
-                    <span className="px-2">1.</span>Tier 2 - 3 Cities
-                  </li>
-                  <li>
-                    <span className="px-2">2.</span>Test for drive
-                  </li>
-                  <li>
-                    <span className="px-2">3.</span>Test for Service Orientation
-                  </li>
-                  <li>
-                    <span className="px-2">4.</span>Test for Intelligence
-                  </li>
-                </ul>
+                <p className="text-base leading-relaxed text-left pt-2">
+                  Digital Transformations requires client senior management
+                  bandwidth for years. Our unique Insights as a Service model
+                  eliminates any need for client teams for Project Management.
+                </p>
               </div>
+
               <div className=" px-6 py-2 rounded-lg">
-                <h2 className="text-xl font-bold mb-2 text-[#090F4A]">
-                  Process Executive & Delivery Manager
+                <h2 className="text-xl font-bold mb-2 bg-[#090F4A] text-white py-3 px-2">
+                  Expertise
                 </h2>
-                <ul className="text-gray-700">
-                  <li>
-                    <p className="font-bold">Internal Certifications:</p>
-                  </li>
-                  <li>
-                    <span className="px-2">1.</span>Process knowledge - Starter
-                  </li>
-                  <li>
-                    <span className="px-2">2.</span>Governance Frameworks -
-                    Starter
-                  </li>
-                  <li>
-                    <span className="px-2">3.</span>Client Communication Skills
-                    - Starter
-                  </li>
-                </ul>
+                <p className="text-base leading-relaxed text-left pt-2">
+                  Digital Transformation can requires deep domain expertise as
+                  well as technology skills. Our unique Insights as a Service
+                  model eliminates any need for clients to possess these skills.
+                </p>
               </div>
+
               <div className=" px-6 py-2 rounded-lg">
-                <h2 className="text-xl font-bold mb-2 text-[#090F4A]">
-                  Account Delivery Manager
+                <h2 className="text-xl font-bold mb-2 bg-[#090F4A] text-white py-3 px-2">
+                  Data
                 </h2>
-                <ul className="text-gray-700">
-                  <li>
-                    <span className="px-2">1.</span>Process knowledge - Advanced
-                  </li>
-                  <li>
-                    <span className="px-2">2.</span>Governance Frameworks -
-                    Advanced
-                  </li>
-                  <li>
-                    <span className="px-2">3.</span>Client Communication Skills
-                    - Advanced
-                  </li>
-                </ul>
+                <p className="text-base leading-relaxed text-left pt-2">
+                  Foundation for Ai readiness is data. Substantial resources &
+                  expertise are needed to aggregate enterprise data, As part of
+                  our unique Insights as a Service model our teams manage the
+                  entire process of building your data layer.
+                </p>
               </div>
+
               <div className=" px-6 py-2 rounded-lg">
-                <h2 className="text-xl font-bold mb-2 text-[#090F4A]">
-                  Pod Leader
+                <h2 className="text-xl font-bold mb-2 bg-[#090F4A] text-white py-3 px-2">
+                  User Adoption
                 </h2>
-                <ul className="text-gray-700">
-                  <li>
-                    <span className="px-2">1.</span>Min 2 Yrs in Account
-                    Delivery
-                  </li>
-                  <li>
-                    <span className="px-2">2.</span>Eligible to open Independent
-                    office in location of choice
-                  </li>
-                  <li>
-                    <span className="px-2">3.</span>Profit share
-                  </li>
-                </ul>
+                <p className="text-base leading-relaxed text-left pt-2">
+                  Majority of Digital Transformations fail due to failure of
+                  User Adoption. This causes loss of investments & compeitive
+                  advantage. Our unique Insights as a Service model integrates
+                  Catapult Capability Centre teams trained & incemtivised for
+                  smooth implementation.
+                </p>
+              </div>
+
+              <div className=" px-6 py-2 rounded-lg">
+                <h2 className="text-xl font-bold mb-2 bg-[#090F4A] text-white py-3 px-2">
+                  Speed
+                </h2>
+                <p className="text-base leading-relaxed text-left pt-2">
+                  Digital Transformation can take years to implement allowing
+                  compeitiion and large companies an advantage. Our unique
+                  Insights as a Service model eliminates this by bringing
+                  pre-built assets, frameworks, algorithim libraries & trained
+                  teams.
+                </p>
               </div>
             </div>
           </div>
-          
         </div>
       </div>
     </>
